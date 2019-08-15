6a7d8c4db38e3154c125b560ef56de2779e129ed
chrissayon <Christopher.L.Sayon@gmail.com>
2019-08-15 18:45:07 +1000

    Created the models for the sdn ryu apis

--- sdn_communication/admin.py
+++ sdn_communication/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import SwitchHardware
+from .models import Switch, DescStats, FlowStats, FlowAggregateStats, TableStats, PortStats 
 # Register your models here.
 
-admin.site.register(SwitchHardware)
+admin.site.register(Switch)
+admin.site.register(DescStats)
+admin.site.register(FlowStats)
+admin.site.register(FlowAggregateStats)
+admin.site.register(TableStats)
+admin.site.register(PortStats)
--- sdn_communication/models.py
+++ sdn_communication/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class SwitchHardware(models.Model):
+class DescStats(models.Model):
     '''Model for hardware description'''
     dp_desc = models.CharField(default = "", max_length = 50)
     mfr_desc = models.CharField(default = "", max_length = 50)
@@ -9,8 +9,56 @@ class SwitchHardware(models.Model):
     serial_num = models.CharField(default = "", max_length = 50)
     dp_desc= models.CharField(default = "", max_length = 50)
 
+class FlowStats(models.Model):
+    '''Flow Table information'''
+    dpid = models.IntegerField(default = 0)
+    actions = models.CharField(default = "", max_length = 50)
+    idle_timeout = models.IntegerField(default = 0)
+    cookie = models.IntegerField(default = 0)
+    packet_count = models.IntegerField(default = 0)
+    hard_timeout = models.IntegerField(default = 0)
+    byte_count = models.IntegerField(default = 0)
+    duration_sec = models.IntegerField(default = 0)
+    duration_nsec = models.IntegerField(default = 0)
+    priority = models.IntegerField(default = 0)
+    length = models.IntegerField(default = 0)
+    flags = models.IntegerField(default = 0)
+    table_id = models.IntegerField(default = 0)
+    actions = models.CharField(default = "", max_length = 50)
+
+class FlowAggregateStats(models.Model):
+    '''Total amount of packets sent'''
+    dpid = models.IntegerField(default = 0)
+    packet_count = models.IntegerField(default = 0)
+    byte_count = models.IntegerField(default = 0)
+    flow_count = models.IntegerField(default = 0)
+
+class TableStats(models.Model):
+    '''Need to resolve the redundant tables that go over 100+'''
+    dpid = models.IntegerField(default = 0)
+    table_id = models.IntegerField(default = 0)
+    matched_count = models.IntegerField(default = 0)
+    lookup_count = models.IntegerField(default = 0)
+    active_count = models.IntegerField(default = 0)
+
+class PortStats(models.Model):
+    '''Statistics on the respective port'''
+    port_number = models.IntegerField(default = 0)
+    rx_packets = models.IntegerField(default = 0)
+    rx_crc_err = models.IntegerField(default = 0)
+    tx_bytes = models.IntegerField(default = 0)
+    rx_dropped = models.IntegerField(default = 0)
+    port_no = models.CharField(default = "", max_length = 50)
+    rx_over_err = models.IntegerField(default = 0)
+    rx_frame_err = models.IntegerField(default = 0)
+    rx_bytes = models.IntegerField(default = 0)
+    tx_errors = models.IntegerField(default = 0)
+    duration_nsec = models.IntegerField(default = 0)
+    collisions = models.IntegerField(default = 0)
+    duration_sec = models.IntegerField(default = 0)
+    rx_errors = models.IntegerField(default = 0)
+    tx_packets = models.IntegerField(default = 0)
 
 # # Create your models here.
 class Switch(models.Model):
     switch_number = models.IntegerField(default=0)
-    switch_id = models.IntegerField(default=0)
