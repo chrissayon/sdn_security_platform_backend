48687a7e8d76476b57217eca59e5f2fefa677b4c
chrissayon <Christopher.L.Sayon@gmail.com>
2019-08-16 20:13:30 +1000

    Datetime field migrations done

--- sdn_communication/models.py
+++ sdn_communication/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-
 class DescStats(models.Model):
     '''Model for hardware description'''
     dp_desc       = models.CharField(default = "", max_length = 50)
@@ -9,8 +8,8 @@ class DescStats(models.Model):
     hw_desc       = models.CharField(default = "", max_length = 50)
     sw_desc       = models.CharField(default = "", max_length = 50)
     serial_num    = models.CharField(default = "", max_length = 50)
-    created       = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
+    created       = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True)
 
 
 class FlowStats(models.Model):
@@ -29,8 +28,8 @@ class FlowStats(models.Model):
     flags         = models.IntegerField(default = 0)
     table_id      = models.IntegerField(default = 0)
     match         = models.CharField(default = "", max_length = 50)
-    created       = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
+    created       = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True)
     
 
 class FlowAggregateStats(models.Model):
@@ -39,8 +38,8 @@ class FlowAggregateStats(models.Model):
     packet_count  = models.IntegerField(default = 0)
     byte_count    = models.IntegerField(default = 0)
     flow_count    = models.IntegerField(default = 0)
-    created       = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
+    created       = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True)
 
 
 class TableStats(models.Model):
@@ -50,8 +49,8 @@ class TableStats(models.Model):
     matched_count = models.IntegerField(default = 0)
     lookup_count  = models.IntegerField(default = 0)
     active_count  = models.IntegerField(default = 0)
-    created       = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
+    created       = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True)
 
 
 class PortStats(models.Model):
@@ -73,8 +72,8 @@ class PortStats(models.Model):
     duration_sec  = models.IntegerField(default = 0)
     rx_errors     = models.IntegerField(default = 0)
     tx_packets    = models.IntegerField(default = 0)
-    created       = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
+    created       = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True)
 
 
 class Switch(models.Model):
